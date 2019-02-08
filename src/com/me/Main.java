1339ede14ec6c44034f95b0640f51d303230ee9f
FalahHAbbas <45371741+FalahHAbbas@users.noreply.github.com>
2019-02-08 11:31:36 +0300

    Update Main.java

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -1,8 +1,39 @@
 package com.me;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        int len = new Scanner(System.in).nextInt();
+        int[] init = new int[len];
+        for (int i = 0; i < len; i++) {
+            init[i] = i+1;
+        }
+        round(init);
+    }
+
+    private static void round(int[] arr) {
+        if (arr.length == 1) {
+            System.out.println("arr = " + arr[0]);
+            return;
+        }
+        int[] newArr = new int[arr.length / 2];
+        if (arr.length % 2 == 0) {
+            int c = 0;
+            for (int i = 0; i < arr.length; i++) {
+                if (i % 2 == 0) {
+                    newArr[c++] = arr[i];
+                }
+            }
+        } else {
+            int c = 0;
+            for (int i = 1; i < arr.length; i++) {
+                if (i % 2 != 0) {
+                    newArr[c++] = arr[i+1];
+                }
+            }
+        }
+        round(newArr);
     }
 }
